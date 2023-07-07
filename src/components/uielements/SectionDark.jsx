ff11ce7897ae30dbd8edb6b1f5ad2c02758398ea
abhkrs <winabhi.ak@gmail.com>
2023-07-07 20:00:42 +0530

    Font and SIde Gap Updated

--- src/components/uielements/SectionDark.jsx
+++ src/components/uielements/SectionDark.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SectionDark({ bg, className, children }) {
   return (
     <section className="bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url('/images/bglgr.png')` }}>
-      <div className={`${className} 2xl:container p-6 lg:px-10 mx-auto`}>{children}</div>
+      <div className={`${className} 2xl:container p-6 md:px-10 lg:px-20 mx-auto`}>{children}</div>
     </section>
   );
 }
