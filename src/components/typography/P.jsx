c8be0bd1f7d02c2dc5a329fb76e6099fab58076d
abhkrs <winabhi.ak@gmail.com>
2023-07-07 21:02:59 +0530

    body text gray

--- src/components/typography/P.jsx
+++ src/components/typography/P.jsx
@@ -1,6 +1,6 @@
 function P({ children, className }) {
     return (
-      <p className={`${className} text-sm md:text-md lg:text-base`}>{children}</p>
+      <p className={`${className} text-sm md:text-md lg:text-base text-gray-600`}>{children}</p>
     );
   }
   
