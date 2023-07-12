a1989761b37698756cd104e9ed1086b3ac52dd89
abhkrs <winabhi.ak@gmail.com>
2023-07-12 21:49:39 +0530

    Sidepanel sticky

--- src/app/courses/workshop-with-internship/page.jsx
+++ src/app/courses/workshop-with-internship/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import React, { useState, useEffect, useRef } from "react";
 import Testimonials from "@/components/sections/Testimonials";
@@ -37,15 +37,13 @@ export default function Page() {
 
       if (sideTile && testimonialSection) {
         const sideTileRect = sideTile.getBoundingClientRect();
-        const testimonialSectionRect =
-          testimonialSection.getBoundingClientRect();
+        const testimonialSectionRect = testimonialSection.getBoundingClientRect();
 
         if (sideTileRect.top <= 0 && testimonialSectionRect.top > 0) {
           sideTile.style.position = "fixed";
           sideTile.style.top = "8px";
         } else {
           sideTile.style.position = "absolute";
-          sideTile.style.top = "auto";
         }
       }
     };
@@ -60,7 +58,7 @@ export default function Page() {
     <main>
       <SectionDark className="flex pt-12 pb-8 justify-between relative max-h-min">
         <div className="w-2/3">
-          <H2 className="!text-white !text-4xl inter !font-inter">
+        <H2 className="!text-white !text-4xl inter !font-inter">
             UI/UX Design with 100% Paid Internship
           </H2>
           <P className="my-6 !text-white !text-lg">
@@ -93,8 +91,8 @@ export default function Page() {
             <span className="font-bold">1,277 </span> people already enrolled
           </P>
         </div>
-        <div className="w-1/3 absolute right-8 top-8 z-40">
-          <div className="bg-white shadow-md p-6">
+        <div className="w-1/3 z-40 relative -top-10 z-50">
+        <div className="bg-white shadow-md p-6 fixed mr-10" ref={sideTileRef}>
             <H3 className="!font-semibold !text-xl">
               Live Mentorship Guidance
             </H3>
@@ -158,215 +156,218 @@ export default function Page() {
           </div>
         </div>
       </SectionDark>
+
       {callBackForm && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-75 z-50 flex items-center justify-center">
-          <div className="bg-white p-8 rounded lg:px-16 shadow-lg w-2/3 relative">
-            <H3 className="!text-2xl">
-              😇 Thank You For Your Callback Request!{" "}
-            </H3>
-            <P className="my-3 !text-xl">Please help us with a few details</P>
-            <hr />
-            <button
-              onClick={() => {
-                setCallBackForm(false);
-              }}
-              className="absolute top-4 right-4 text-red-500"
-            >
-              <Image alt="" src="/images/close.svg" height="18" width="18" />
-            </button>
-            <form onSubmit={handleSubmit} className="relative mt-4 max-w-max">
-              <div className="mb-4">
-                <input
-                  type="text"
-                  id="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Your Full Name*"
-                  className="border border-gray-300 px-3  py-1 w-full rounded "
-                />
-              </div>
-              <div className="mb-4">
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Your Email ID*"
-                  className="border border-gray-300 px-3  py-1 w-full rounded "
-                />
-              </div>
-              <div className="mb-4 flex">
-                <div className="border border-gray-300 pl-3 pr-2  py-1 rounded-l border-r-0 !text-gray-600 font-semibold">
-                  +91
-                </div>
-                <span className="text-gray-400 border-t border-b py-1 border-gray-300">
-                  |
-                </span>
-                <input
-                  type="tel"
-                  id="phone"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  placeholder="Your Mobile Number*"
-                  className="border border-gray-300 pl-2 pr-3  py-1 w-full rounded-r border-l-0 "
-                />
-              </div>
-              <div className="mb-4">
-                <select
-                  id="timeSlot"
-                  value={timeSlot}
-                  onChange={(e) => setTimeSlot(e.target.value)}
-                  className="border border-gray-300 px-3  py-1 w-full rounded "
-                >
-                  <option value="" disabled selected>
-                    Select a time slot
-                  </option>
-                  <option value="Today, 10th May, 2 PM - 3 PM">
-                    Today, 10th May, 2 PM - 3 PM
-                  </option>
-                  <option value="Today, 10th May, 2 PM - 3 PM">
-                    Today, 10th May, 2 PM - 3 PM
-                  </option>
-                  <option value="Tomorrow, 10th May, 10 AM - 12 PM">
-                    Tomorrow, 10th May, 10 AM - 12 PM
-                  </option>
-                  <option value="Tomorrow, 10th May, 12 PM - 2 PM">
-                    Tomorrow, 10th May, 12 PM - 2 PM
-                  </option>
-                  <option value="Tomorrow, 10th May, 2 PM - 4 PM">
-                    Tomorrow, 10th May, 2 PM - 4 PM
-                  </option>
-                </select>
-              </div>
-              <button
-                type="submit"
-                className="bg-black !text-white py-2 px-8 rounded !text-xl rounded-full hover:bg-primary"
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
+         <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-75 z-50 flex items-center justify-center">
+         <div className="bg-white p-8 rounded lg:px-16 shadow-lg w-2/3 relative">
+           <H3 className="!text-2xl">
+             😇 Thank You For Your Callback Request!{" "}
+           </H3>
+           <P className="my-3 !text-xl">Please help us with a few details</P>
+           <hr />
+           <button
+             onClick={() => {
+               setCallBackForm(false);
+             }}
+             className="absolute top-4 right-4 text-red-500"
+           >
+             <Image alt="" src="/images/close.svg" height="18" width="18" />
+           </button>
+           <form onSubmit={handleSubmit} className="relative mt-4 max-w-max">
+             <div className="mb-4">
+               <input
+                 type="text"
+                 id="name"
+                 value={name}
+                 onChange={(e) => setName(e.target.value)}
+                 placeholder="Your Full Name*"
+                 className="border border-gray-300 px-3  py-1 w-full rounded "
+               />
+             </div>
+             <div className="mb-4">
+               <input
+                 type="email"
+                 id="email"
+                 value={email}
+                 onChange={(e) => setEmail(e.target.value)}
+                 placeholder="Your Email ID*"
+                 className="border border-gray-300 px-3  py-1 w-full rounded "
+               />
+             </div>
+             <div className="mb-4 flex">
+               <div className="border border-gray-300 pl-3 pr-2  py-1 rounded-l border-r-0 !text-gray-600 font-semibold">
+                 +91
+               </div>
+               <span className="text-gray-400 border-t border-b py-1 border-gray-300">
+                 |
+               </span>
+               <input
+                 type="tel"
+                 id="phone"
+                 value={phone}
+                 onChange={(e) => setPhone(e.target.value)}
+                 placeholder="Your Mobile Number*"
+                 className="border border-gray-300 pl-2 pr-3  py-1 w-full rounded-r border-l-0 "
+               />
+             </div>
+             <div className="mb-4">
+               <select
+                 id="timeSlot"
+                 value={timeSlot}
+                 onChange={(e) => setTimeSlot(e.target.value)}
+                 className="border border-gray-300 px-3  py-1 w-full rounded "
+               >
+                 <option value="" disabled selected>
+                   Select a time slot
+                 </option>
+                 <option value="Today, 10th May, 2 PM - 3 PM">
+                   Today, 10th May, 2 PM - 3 PM
+                 </option>
+                 <option value="Today, 10th May, 2 PM - 3 PM">
+                   Today, 10th May, 2 PM - 3 PM
+                 </option>
+                 <option value="Tomorrow, 10th May, 10 AM - 12 PM">
+                   Tomorrow, 10th May, 10 AM - 12 PM
+                 </option>
+                 <option value="Tomorrow, 10th May, 12 PM - 2 PM">
+                   Tomorrow, 10th May, 12 PM - 2 PM
+                 </option>
+                 <option value="Tomorrow, 10th May, 2 PM - 4 PM">
+                   Tomorrow, 10th May, 2 PM - 4 PM
+                 </option>
+               </select>
+             </div>
+             <button
+               type="submit"
+               className="bg-black !text-white py-2 px-8 rounded !text-xl rounded-full hover:bg-primary"
+             >
+               Submit
+             </button>
+           </form>
+         </div>
+       </div>
       )}
+
       {registerCourseForm && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-75 z-50 flex items-center justify-center">
-          <div className="bg-white p-8 rounded shadow-lg w-2/3 relative lg:px-16">
-            <H3 className="!text-2xl mb-4">🧑🏽‍🎓 Register for Course</H3>
-            <button
-              onClick={() => {
-                setRegisterCourseForm(false);
-              }}
-              className="absolute top-4 right-4 text-red-500"
-            >
-              <Image alt="" src="/images/close.svg" height="18" width="18" />
-            </button>
-            <hr />
-            <P className="mt-4 bg-gold px-2 py-1 !text-white max-w-max">
-              Your Batch will start from July
-            </P>
+         <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-75 z-50 flex items-center justify-center">
+         <div className="bg-white p-8 rounded shadow-lg w-2/3 relative lg:px-16">
+           <H3 className="!text-2xl mb-4">🧑🏽‍🎓 Register for Course</H3>
+           <button
+             onClick={() => {
+               setRegisterCourseForm(false);
+             }}
+             className="absolute top-4 right-4 text-red-500"
+           >
+             <Image alt="" src="/images/close.svg" height="18" width="18" />
+           </button>
+           <hr />
+           <P className="mt-4 bg-gold px-2 py-1 !text-white max-w-max">
+             Your Batch will start from July
+           </P>
 
-            <form onSubmit={handleSubmit} className="relative mt-4">
-              <div className="mb-4">
-                <input
-                  type="text"
-                  id="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Your Full Name*"
-                  className="border border-gray-300 px-3  py-1 w-1/2 rounded "
-                />
-              </div>
-              <div className="mb-4">
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Your Email ID*"
-                  className="border border-gray-300 px-3  py-1 w-1/2 rounded "
-                />
-              </div>
-              <div className="mb-4">
-                <P className="capitalize text-black font-semibold">
-                  Select from Our available batches
-                </P>
-                <div className="gap-4 flex flex-wrap my-2">
-                  <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
-                    Mon (7PM - 8:30PM)
-                  </button>
-                  <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
-                    Mon (7PM - 8:30PM)
-                  </button>
-                  <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
-                    Mon (7PM - 8:30PM)
-                  </button>
-                  <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
-                    Mon (7PM - 8:30PM)
-                  </button>
-                  <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
-                    Mon (7PM - 8:30PM)
-                  </button>
-                  <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
-                    Mon (7PM - 8:30PM)
-                  </button>
-                </div>
-              </div>
-              <div className="mb-3 mt-6">
-                <input
-                  type="radio"
-                  id="ux-certificate"
-                  checked={uxCertificate}
-                  onChange={() => {
-                    setInternship(false);
-                    setUXCertificate(!uxCertificate);
-                    setPayableAmount(3000);
-                  }}
-                  className="mr-2"
-                />
-                <label
-                  htmlFor="ux-certificate"
-                  className={uxCertificate ? "font-semibold" : ""}
-                >
-                  UI/UX Certification @ INR 1,000 per month only
-                </label>
-              </div>
-              <div className="mb-6">
-                <input
-                  type="radio"
-                  id="internship"
-                  checked={internship}
-                  onChange={() => {
-                    setUXCertificate(false);
-                    setInternship(!internship);
-                    setPayableAmount(4200);
-                  }}
-                  className="mr-2"
-                />
-                <label
-                  htmlFor="internship"
-                  className={internship ? "font-semibold" : ""}
-                >
-                  UI/UX Certification with Paid Internship @ INR 2,200 per month
-                  only
-                </label>
-              </div>
+           <form onSubmit={handleSubmit} className="relative mt-4">
+             <div className="mb-4">
+               <input
+                 type="text"
+                 id="name"
+                 value={name}
+                 onChange={(e) => setName(e.target.value)}
+                 placeholder="Your Full Name*"
+                 className="border border-gray-300 px-3  py-1 w-1/2 rounded "
+               />
+             </div>
+             <div className="mb-4">
+               <input
+                 type="email"
+                 id="email"
+                 value={email}
+                 onChange={(e) => setEmail(e.target.value)}
+                 placeholder="Your Email ID*"
+                 className="border border-gray-300 px-3  py-1 w-1/2 rounded "
+               />
+             </div>
+             <div className="mb-4">
+               <P className="capitalize text-black font-semibold">
+                 Select from Our available batches
+               </P>
+               <div className="gap-4 flex flex-wrap my-2">
+                 <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
+                   Mon (7PM - 8:30PM)
+                 </button>
+                 <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
+                   Mon (7PM - 8:30PM)
+                 </button>
+                 <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
+                   Mon (7PM - 8:30PM)
+                 </button>
+                 <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
+                   Mon (7PM - 8:30PM)
+                 </button>
+                 <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
+                   Mon (7PM - 8:30PM)
+                 </button>
+                 <button className="py-1 px-3 bg-gray-100 border-secondary border rounded min-w-max">
+                   Mon (7PM - 8:30PM)
+                 </button>
+               </div>
+             </div>
+             <div className="mb-3 mt-6">
+               <input
+                 type="radio"
+                 id="ux-certificate"
+                 checked={uxCertificate}
+                 onChange={() => {
+                   setInternship(false);
+                   setUXCertificate(!uxCertificate);
+                   setPayableAmount(3000);
+                 }}
+                 className="mr-2"
+               />
+               <label
+                 htmlFor="ux-certificate"
+                 className={uxCertificate ? "font-semibold" : ""}
+               >
+                 UI/UX Certification @ INR 1,000 per month only
+               </label>
+             </div>
+             <div className="mb-6">
+               <input
+                 type="radio"
+                 id="internship"
+                 checked={internship}
+                 onChange={() => {
+                   setUXCertificate(false);
+                   setInternship(!internship);
+                   setPayableAmount(4200);
+                 }}
+                 className="mr-2"
+               />
+               <label
+                 htmlFor="internship"
+                 className={internship ? "font-semibold" : ""}
+               >
+                 UI/UX Certification with Paid Internship @ INR 2,200 per month
+                 only
+               </label>
+             </div>
 
-              <P className="italic ms-2 mb-2">
-                Currently you are paying fee for 1st month of your course
-              </P>
+             <P className="italic ms-2 mb-2">
+               Currently you are paying fee for 1st month of your course
+             </P>
 
-              <button
-                type="submit"
-                className="bg-black w-2/3 !text-white pb-2 pt-1 px-8 rounded !text-xl rounded-full hover:bg-primary"
-              >
-                Proceed to pay {payableAmount}
-              </button>
-            </form>
-          </div>
-        </div>
+             <button
+               type="submit"
+               className="bg-black w-2/3 !text-white pb-2 pt-1 px-8 rounded !text-xl rounded-full hover:bg-primary"
+             >
+               Proceed to pay {payableAmount}
+             </button>
+           </form>
+         </div>
+       </div>
       )}
+
       <Section className="my-6 relative">
-        <div className="w-2/3 grid gap-8 pr-8">
+      <div className="w-2/3 grid gap-8 pr-8">
           <div className="bg-white px-12 py-10 rounded shadow ">
             <H3 className="!text-2xl">About The Course </H3>
             <P className="text-gray-600 mt-4 !text-base">
@@ -666,8 +667,9 @@ export default function Page() {
           </div>
         </div>
       </Section>
+
       <SectionDark>
-        <div className="w-2/3 !text-white py-6">
+      <div className="w-2/3 !text-white py-6">
           <H2 className="!text-4xl !font-normal">
             Earn a certificate in UI/UX design along with Internship
           </H2>
@@ -683,7 +685,8 @@ export default function Page() {
           </Link>
         </div>
       </SectionDark>
-      <Testimonials />
+
+      <Testimonials ref={testimonialSectionRef} />
     </main>
   );
 }
