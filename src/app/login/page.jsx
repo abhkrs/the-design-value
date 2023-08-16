29898fb53f6840b4c52852ed2d29dc56d33de2e4
Abhishek <winabhi.ak@gmail.com>
2023-08-17 03:24:14 +0530

    localstorege error

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -11,8 +11,7 @@ export default function Page() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const router = useRouter();
-
+const router = useRouter();
   const handleLogin = async () => {
     try {
       const response = await fetch("https://aj2709.pythonanywhere.com/Register/login", {
@@ -20,26 +19,29 @@ export default function Page() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
       });
-
+  
       const data = await response.json();
-
+  
       if (response.ok) {
+        const authData = {
+          isLoggedIn: true,
+          isSuperuser: data.is_superuser,
+          studentDetails: data.StudentData
+        };
+        localStorage.setItem('authData', JSON.stringify(authData));
+        
         if (data.is_superuser) {
-          localStorage.setItem('authData', JSON.stringify({ isLoggedIn: true, isSuperuser: true }));
           router.push('/dashboard');
         } else {
-          localStorage.setItem('authData', JSON.stringify({ isLoggedIn: true, isSuperuser: false, studentDetails: data.StudentData }));
           router.push('/profile');
         }
       } else {
         setError("Invalid credentials");
       }
-
     } catch (err) {
       setError("Error logging in");
     }
   };
-
 
   return (
     <main>
