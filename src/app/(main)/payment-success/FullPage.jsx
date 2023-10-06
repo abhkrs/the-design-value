649c67641f4ed958863b80fbad7cb8e5f05d6ce7
Tanmaydey2023 <tanmaydey.roldrive@gmail.com>
2023-10-06 10:37:11 +0530

    [FIX] Failed payment text and image

--- src/app/(main)/payment-success/FullPage.jsx
+++ src/app/(main)/payment-success/FullPage.jsx
@@ -53,11 +53,14 @@ export default function FullPage() {
               console.log(responseData);
 
               if (responseData.code === "PAYMENT_SUCCESS") {
-                const payentPayload ={
+                const payentPayload = {
                   paymentId: orderId,
-                  paymentStatus: 1
-                }
-                const paymentResponse = await api.post("/Register/payupdate", payentPayload);
+                  paymentStatus: 1,
+                };
+                const paymentResponse = await api.post(
+                  "/Register/payupdate",
+                  payentPayload
+                );
                 console.log(paymentResponse);
                 if (paymentResponse.status === "Success") {
                   toast.success(paymentResponse?.message, {
@@ -109,7 +112,11 @@ export default function FullPage() {
         //     console.error("Error:", error);
         //     setShowLoader(false);
         //   });
+      } else {
+        router.back();
       }
+    } else {
+      router.back();
     }
   }, [router]);
 
@@ -148,21 +155,17 @@ export default function FullPage() {
         <>
           <div className="relative mx-auto mt-20 w-24 h-24">
             <Image
-              src="/images/wow.png"
+              src="/images/oops.svg"
               fill={true}
               alt="Payment done"
               className="object-contain w-full h-full"
             />
           </div>
-          <H2 className="text-center !text-black mb-5 md:mt-2">
-            Payment Failed
-          </H2>
-          <P className="text-center !text-secondary my-2 !text-xl">
-            Your payment has been successful.
+
+          <P className="text-center !text-[#AF2600] my-2 !text-2xl">
+            Your payment has failed!
           </P>
-          <P className="text-center !text-lg">
-            Email has been sent to you with your student Log In details.
-          </P>
+          <P className="text-center !text-lg">Please try again...</P>
           <P className="text-center mt-12">
             <Link
               className="bg-black hover:bg-secondary !text-white px-6 sm:px-20 py-2 text-xl rounded-full"
